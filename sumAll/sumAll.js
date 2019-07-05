972cdc2e4452aa58db90ad04101de1807af8ef32
halftruth <52118055+halftruth@users.noreply.github.com>
2019-07-04 21:36:38 -0500

    Solve sumAll exercise

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(x,y) {
+    let sum = 0;
+    let greater;
+    let lesser;
+    if (typeof x != 'number' || typeof y != 'number') {
+        return 'ERROR';
+    } else if ((x < 0) || (y < 0)) {
+        return 'ERROR';
+    } else if (x == y) {
+        return sum;
+    } else if (x > y) {
+        greater = x;
+        lesser = y;
+    } else {
+        greater = y;
+        lesser = x;
+    }
 
+    for (let i = lesser; i <= greater; i++) {
+        sum += i;
+    }
+    return sum;
 }
 
 module.exports = sumAll
