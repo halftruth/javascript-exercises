61ee38e6c613082fecfafc5582048081771058c1
halftruth <52118055+halftruth@users.noreply.github.com>
2019-07-04 20:48:42 -0500

    Solve exercises 1 and 2

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, x) {
+    let stringRepeated = "";
+    if (x < 0) {
+        return "ERROR";
+    }
+    for (let i = 0; i < x; i++){
+        stringRepeated += string;
+    }
+    return stringRepeated;
 }
 
 module.exports = repeatString
