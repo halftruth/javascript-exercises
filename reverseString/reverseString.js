797ce1d5b82268fe4b175c829c6c45fed8cf47cd
halftruth <52118055+halftruth@users.noreply.github.com>
2019-07-04 20:53:14 -0500

    Solve reverseString exercise

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let stringChars = string.split("");
+    let stringReversed = "";
 
+    for (let i = stringChars.length - 1; i >= 0; i--) {
+        stringReversed += stringChars[i];
+    }
+    return stringReversed;
 }
 
 module.exports = reverseString
