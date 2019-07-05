7531c44f026b212cadccfbeaa0ffdfb35eccb24f
halftruth <52118055+halftruth@users.noreply.github.com>
2019-07-04 21:27:53 -0500

    Solve removeFromArray exercise

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    let lengthOfArray = array.length;
+    let numberOfArgs = arguments.length;
 
+    for (let i = 1; i < numberOfArgs; i++){
+        let removal = arguments[i];
+        for (let j = 0; j < lengthOfArray; j++) {
+            let removalCandidate = array[j];
+            if (removal == removalCandidate) {
+                array.splice(j,1);
+            } else {
+                continue;
+            }
+        }
+    }
+    return array;
 }
 
 module.exports = removeFromArray
