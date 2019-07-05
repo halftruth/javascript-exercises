a20d92b245064e9d8bc89a86199513fb1f2928ef
halftruth <52118055+halftruth@users.noreply.github.com>
2019-07-05 07:26:21 -0500

    Solve tempConversion exercise

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(tempInFahrenheit) {
+  let tempInCelsius = Math.round(((tempInFahrenheit - 32)*(5/9)) * 10) / 10;
+  return tempInCelsius;
 }
 
-const ctof = function() {
-
+const ctof = function(tempInCelsius) {
+  let tempInFahrenheit = Math.round((tempInCelsius * (9/5) + 32)*10) / 10; // rounds to 1 decimal
+  return tempInFahrenheit;
 }
 
 module.exports = {
